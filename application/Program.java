b9b91b10272a7c94acba4879fdde2719c3cd43d3
higoramorim <1810042@aluno.univesp.br>
2021-05-08 15:19:06 -0300

    findById implementation

--- application/Program.java
+++ application/Program.java
@@ -11,12 +11,16 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		/*
 		Department obj = new Department(1, "Books");
 		
 		
 		Seller seller = new Seller(10, "Davi", "davi@gmail.com", new Date(), 7500.0, obj);
+		*/
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
