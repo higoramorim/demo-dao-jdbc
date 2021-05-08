facaf4aa48729c35f88c89f4a2295b1cbe88696e
higoramorim <1810042@aluno.univesp.br>
2021-05-08 14:26:52 -0300

    seller entity class

--- application/Program.java
+++ application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		
+		Seller seller = new Seller(10, "Davi", "davi@gmail.com", new Date(), 7500.0, obj);
+		System.out.println(seller);
 	}
 
 }
